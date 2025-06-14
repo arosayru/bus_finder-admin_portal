7a174943ef8e1c20b002a323d0c91604552c1798
Adithya Rosayru <arosayru@gmail.com>
2025-06-14 21:43:12 +0530

    Update Add Staff Modal

--- src/components/AddStaffModal.js
+++ src/components/AddStaffModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaEye, FaUserCircle, FaUpload } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaUserCircle, FaUpload } from 'react-icons/fa';
 
 const AddStaffModal = ({ onClose }) => {
   const [showPass, setShowPass] = useState(false);
@@ -78,10 +78,16 @@ const AddStaffModal = ({ onClose }) => {
               placeholder="Password"
               className="w-full p-3 pr-10 rounded-md bg-orange-50 placeholder-[#7E7573] text-black focus:outline-none"
             />
-            <FaEye
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#BD2D01] cursor-pointer"
+            <div
               onClick={() => setShowPass(!showPass)}
-            />
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            >
+              {showPass ? (
+                <FaEyeSlash className="text-[#BD2D01]" />
+              ) : (
+                <FaEye className="text-[#BD2D01]" />
+              )}
+            </div>
           </div>
 
           {/* Confirm Password */}
@@ -91,10 +97,16 @@ const AddStaffModal = ({ onClose }) => {
               placeholder="Confirm Password"
               className="w-full p-3 pr-10 rounded-md bg-orange-50 placeholder-[#7E7573] text-black focus:outline-none"
             />
-            <FaEye
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#BD2D01] cursor-pointer"
+            <div
               onClick={() => setShowConfirm(!showConfirm)}
-            />
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            >
+              {showConfirm ? (
+                <FaEyeSlash className="text-[#BD2D01]" />
+              ) : (
+                <FaEye className="text-[#BD2D01]" />
+              )}
+            </div>
           </div>
 
           {/* Add Button Right Aligned */}

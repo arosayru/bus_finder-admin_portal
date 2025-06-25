cc5bfce64ebd38a2bc0908909b2ae1cda8aa300c
Adithya Rosayru <arosayru@gmail.com>
2025-06-25 23:02:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://bus-finder-sl-a7c6a549fbb1.herokuapp.com/api'; // Replace with your actual URL
+const API_BASE_URL = 'https://bus-finder-sl-a7c6a549fbb1.herokuapp.com/api'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
